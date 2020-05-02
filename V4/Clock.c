12d1ae7e1d09b53020c319c69df37e022e06c509
Cristto <cristotrader@gmail.com>
2020-05-02 16:59:14 +0200

    v4 entera con un bucle de los primeros ejercicios

---
+++ V4/Clock.c
@@ -0,0 +1,27 @@
+#include "Clock.h"
+#include "Processor.h"
+#include "ComputerSystemBase.h"
+
+int tics = 0;
+
+void Clock_Update()
+{
+
+	tics++;
+
+	// V2 Ej 2.d
+	//ex-n
+	if ((tics > 0) && ((tics % intervalBetweenInterrupts) == 0))
+	{
+		Processor_RaiseInterrupt(CLOCKINT_BIT);
+	}
+	//end ex-n
+
+	// ComputerSystem_DebugMessage(97,CLOCK,tics);v
+}
+
+int Clock_GetTime()
+{
+
+	return tics;
+}
